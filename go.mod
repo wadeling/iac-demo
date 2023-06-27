7b4f95ddfc2fd3c9c6343fc1b99496c757adffca
lingximo <lingximo@tensorsecurity.cn>
2023-06-27 16:46:47 +0800

    run ok

---
+++ go.mod
@@ -0,0 +1,90 @@
+module github.com/wadeling/iac-demo
+
+go 1.20
+
+require (
+	github.com/aquasecurity/defsec v0.89.0
+	github.com/aquasecurity/trivy v0.42.1
+	github.com/aquasecurity/trivy-db v0.0.0-20230626135706-6e8ee5a37ad9
+	github.com/liamg/memoryfs v1.6.0
+	github.com/open-policy-agent/opa v0.53.1
+	github.com/samber/lo v1.38.1
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/OneOfOne/xxhash v1.2.8 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230518184743-7afd39499903 // indirect
+	github.com/acomagu/bufpipe v1.0.4 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/anchore/go-struct-converter v0.0.0-20221118182256-c68fdcfa2092 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/containerd/typeurl v1.0.2 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/docker/docker v23.0.5+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.4.1 // indirect
+	github.com/go-git/go-git/v5 v5.7.0 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-containerregistry v0.14.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.14.1 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/liamg/iamgo v0.0.9 // indirect
+	github.com/liamg/jfather v0.0.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/buildkit v0.11.5 // indirect
+	github.com/owenrumney/squealer v1.1.1 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.2 // indirect
+	github.com/skeema/knownhosts v1.1.1 // indirect
+	github.com/spdx/tools-golang v0.5.0 // indirect
+	github.com/tchap/go-patricia/v2 v2.3.1 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/yashtewari/glob-intersection v0.1.0 // indirect
+	github.com/zclconf/go-cty v1.10.0 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.14.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,341 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aquasecurity/defsec/pkg/scan"
+	"github.com/aquasecurity/defsec/pkg/scanners/dockerfile"
+	"github.com/aquasecurity/defsec/pkg/scanners/options"
+	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/types"
+	"github.com/liamg/memoryfs"
+	"github.com/open-policy-agent/opa/bundle"
+	"github.com/samber/lo"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func main() {
+	log.Println("start")
+
+	results, err := localFsScan()
+	if err != nil {
+		log.Printf("local fs scan err:%v", err)
+		return
+	}
+	//results, err := memFsScan()
+	//if err != nil {
+	//	log.Printf("memfs scan err.%v", err)
+	//	return
+	//}
+	//log.Printf("results len: %v", len(results))
+
+	misconfs := ResultsToMisconf(ftypes.Dockerfile, "DockerFile", results)
+
+	for _, misconf := range misconfs {
+		sort.Sort(misconf.Successes)
+		sort.Sort(misconf.Warnings)
+		sort.Sort(misconf.Failures)
+	}
+	//log.Printf("misconf:%v", misconfs)
+	//dumpMisconfs(misconfs)
+	res := MisconfsToResults(misconfs)
+	//log.Printf("result %+v", result)
+	data, err := json.MarshalIndent(res, "", "  ")
+	err = os.WriteFile("result.json", data, os.ModePerm)
+	if err != nil {
+		log.Printf("write result failed.%v", err)
+		return
+	}
+	log.Printf("end")
+}
+
+func localFsScan() (scan.Results, error) {
+	scanner := dockerfile.NewScanner(options.ScannerWithEmbeddedPolicies(true))
+	srcFS := os.DirFS("./testdata")
+
+	results, err := scanner.ScanFS(context.Background(), srcFS, ".")
+	return results, err
+}
+
+func memFsScan() (scan.Results, error) {
+	policiesPath, err := filepath.Abs("./rules")
+	log.Printf("polic abs path:%v,base %v", policiesPath, filepath.Base(policiesPath))
+	scanner := dockerfile.NewScanner(
+		options.ScannerWithPolicyDirs(filepath.Base(policiesPath)),
+	)
+
+	memfs := memoryfs.New()
+	err = addFilesToMemFS(memfs, true, policiesPath)
+	if err != nil {
+		log.Printf("failed to add policy files to memfs:%v", err)
+		return nil, err
+	}
+
+	testDataPath, err := filepath.Abs("./testdata")
+	log.Printf("test data path:%v,base %v", testDataPath, filepath.Base(testDataPath))
+	err = addFilesToMemFS(memfs, false, testDataPath)
+	if err != nil {
+		log.Printf("failed to add data files to memfs.%v", err)
+		return nil, err
+	}
+	results, err := scanner.ScanFS(context.Background(), memfs, filepath.Base(testDataPath))
+	if err != nil {
+		log.Printf("scan fs err:%v", err)
+		return nil, err
+	}
+	return results, nil
+}
+
+func addFilesToMemFS(memfs *memoryfs.FS, typePolicy bool, folderName string) error {
+	base := filepath.Base(folderName)
+	if err := memfs.MkdirAll(base, 0o700); err != nil {
+		return err
+	}
+	err := filepath.Walk(filepath.FromSlash(folderName),
+		func(fpath string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Printf("walk err:%v", err)
+				return err
+			}
+			if info.IsDir() {
+				log.Printf("is dir.%v", info.Name())
+				return nil
+			}
+			if typePolicy && !isRegoFile(info.Name()) {
+				log.Printf("not rego file:%v", info.Name())
+				return nil
+			}
+			data, err := os.ReadFile(fpath)
+			if err != nil {
+				log.Printf("read file err %v", err)
+				return err
+			}
+			fileName := getFileName(fpath, info, typePolicy)
+			log.Printf("write file %v to memfs.fpath %v,info %v", fileName, fpath, info.Name())
+			if err := memfs.WriteFile(path.Join(base, fileName), data, 0o644); err != nil {
+				log.Printf("memfs write file err:%v,%v", err, fileName)
+				return err
+			}
+			return nil
+		})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getFileName(fpath string, info os.FileInfo, typePolicy bool) string {
+	pathParts := strings.Split(fpath, filepath.FromSlash("/"))
+	fileName := info.Name()
+	// append test data folder to input file name example Dockerfile.allowed_DS001
+	//if len(pathParts) > 2 && !typePolicy {
+	//	fileName = fmt.Sprintf("%s_%s", fileName, pathParts[len(pathParts)-2])
+	//}
+	log.Printf("filename %v,pathParts %v", fileName, pathParts)
+	return fileName
+}
+
+func isRegoFile(name string) bool {
+	return strings.HasSuffix(name, bundle.RegoExt) && !strings.HasSuffix(name, "_test"+bundle.RegoExt)
+}
+
+func ResultsToMisconf(configType string, scannerName string, results scan.Results) []ftypes.Misconfiguration {
+	misconfs := map[string]ftypes.Misconfiguration{}
+
+	for _, result := range results {
+		flattened := result.Flatten()
+
+		query := fmt.Sprintf("data.%s.%s", result.RegoNamespace(), result.RegoRule())
+
+		ruleID := result.Rule().AVDID
+		if result.RegoNamespace() != "" && len(result.Rule().Aliases) > 0 {
+			ruleID = result.Rule().Aliases[0]
+		}
+
+		cause := NewCauseWithCode(result)
+
+		misconfResult := ftypes.MisconfResult{
+			Namespace: result.RegoNamespace(),
+			Query:     query,
+			Message:   flattened.Description,
+			PolicyMetadata: ftypes.PolicyMetadata{
+				ID:                 ruleID,
+				AVDID:              result.Rule().AVDID,
+				Type:               fmt.Sprintf("%s Security Check", scannerName),
+				Title:              result.Rule().Summary,
+				Description:        result.Rule().Explanation,
+				Severity:           string(flattened.Severity),
+				RecommendedActions: flattened.Resolution,
+				References:         flattened.Links,
+			},
+			CauseMetadata: cause,
+			Traces:        result.Traces(),
+		}
+
+		filePath := flattened.Location.Filename
+		misconf, ok := misconfs[filePath]
+		if !ok {
+			misconf = ftypes.Misconfiguration{
+				FileType: configType,
+				FilePath: filepath.ToSlash(filePath), // defsec return OS-aware path
+			}
+		}
+
+		if flattened.Warning {
+			misconf.Warnings = append(misconf.Warnings, misconfResult)
+		} else {
+			switch flattened.Status {
+			case scan.StatusPassed:
+				misconf.Successes = append(misconf.Successes, misconfResult)
+			case scan.StatusIgnored:
+				misconf.Exceptions = append(misconf.Exceptions, misconfResult)
+			case scan.StatusFailed:
+				misconf.Failures = append(misconf.Failures, misconfResult)
+			}
+		}
+		misconfs[filePath] = misconf
+	}
+
+	return ftypes.ToMisconfigurations(misconfs)
+}
+
+func NewCauseWithCode(underlying scan.Result) ftypes.CauseMetadata {
+	flat := underlying.Flatten()
+	cause := ftypes.CauseMetadata{
+		Resource:  flat.Resource,
+		Provider:  flat.RuleProvider.DisplayName(),
+		Service:   flat.RuleService,
+		StartLine: flat.Location.StartLine,
+		EndLine:   flat.Location.EndLine,
+	}
+	if code, err := underlying.GetCode(); err == nil {
+		cause.Code = ftypes.Code{
+			Lines: lo.Map(code.Lines, func(l scan.Line, i int) ftypes.Line {
+				return ftypes.Line{
+					Number:      l.Number,
+					Content:     l.Content,
+					IsCause:     l.IsCause,
+					Annotation:  l.Annotation,
+					Truncated:   l.Truncated,
+					Highlighted: l.Highlighted,
+					FirstCause:  l.FirstCause,
+					LastCause:   l.LastCause,
+				}
+			}),
+		}
+	}
+	return cause
+}
+
+func dumpMisconfs(misConfs []ftypes.Misconfiguration) {
+	for k, v := range misConfs {
+		log.Printf("index %v, filepath %v", k, v.FilePath)
+		log.Printf("dump failures:\n")
+		for _, f := range v.Failures {
+			log.Printf("failure. start line:%v,end line:%v,resource %v,provider %v,service %v,title %v,des %v",
+				f.StartLine, f.EndLine, f.Resource, f.Provider, f.Service, f.Title, f.Description)
+			for _, l := range f.Code.Lines {
+				log.Printf("code line.number %v,content %v,anotation %v", l.Number, l.Content, l.Annotation)
+			}
+		}
+	}
+}
+
+func MisconfsToResults(misconfs []ftypes.Misconfiguration) types.Results {
+	var results types.Results
+	for _, misconf := range misconfs {
+
+		var detected []types.DetectedMisconfiguration
+
+		for _, f := range misconf.Failures {
+			detected = append(detected, toDetectedMisconfiguration(f, dbTypes.SeverityCritical, types.StatusFailure, misconf.Layer))
+		}
+		for _, w := range misconf.Warnings {
+			detected = append(detected, toDetectedMisconfiguration(w, dbTypes.SeverityMedium, types.StatusFailure, misconf.Layer))
+		}
+		for _, w := range misconf.Successes {
+			detected = append(detected, toDetectedMisconfiguration(w, dbTypes.SeverityUnknown, types.StatusPassed, misconf.Layer))
+		}
+		for _, w := range misconf.Exceptions {
+			detected = append(detected, toDetectedMisconfiguration(w, dbTypes.SeverityUnknown, types.StatusException, misconf.Layer))
+		}
+
+		results = append(results, types.Result{
+			Target:            misconf.FilePath,
+			Class:             types.ClassConfig,
+			Type:              misconf.FileType,
+			Misconfigurations: detected,
+		})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Target < results[j].Target
+	})
+
+	return results
+}
+
+func toDetectedMisconfiguration(res ftypes.MisconfResult, defaultSeverity dbTypes.Severity,
+	status types.MisconfStatus, layer ftypes.Layer) types.DetectedMisconfiguration {
+
+	severity := defaultSeverity
+	sev, err := dbTypes.NewSeverity(res.Severity)
+	if err != nil {
+		log.Printf("severity must be %s, but %s", dbTypes.SeverityNames, res.Severity)
+	} else {
+		severity = sev
+	}
+
+	msg := strings.TrimSpace(res.Message)
+	if msg == "" {
+		msg = "No issues found"
+	}
+
+	var primaryURL string
+
+	// empty namespace implies a go rule from defsec, "builtin" refers to a built-in rego rule
+	// this ensures we don't generate bad links for custom policies
+	if res.Namespace == "" || strings.HasPrefix(res.Namespace, "builtin.") {
+		primaryURL = fmt.Sprintf("https://avd.aquasec.com/misconfig/%s", strings.ToLower(res.ID))
+		res.References = append(res.References, primaryURL)
+	}
+
+	if len(primaryURL) == 0 && len(res.References) > 0 {
+		primaryURL = res.References[0]
+	}
+
+	return types.DetectedMisconfiguration{
+		ID:          res.ID,
+		AVDID:       res.AVDID,
+		Type:        res.Type,
+		Title:       res.Title,
+		Description: res.Description,
+		Message:     msg,
+		Resolution:  res.RecommendedActions,
+		Namespace:   res.Namespace,
+		Query:       res.Query,
+		Severity:    severity.String(),
+		PrimaryURL:  primaryURL,
+		References:  res.References,
+		Status:      status,
+		Layer:       layer,
+		Traces:      res.Traces,
+		CauseMetadata: ftypes.CauseMetadata{
+			Resource:  res.Resource,
+			Provider:  res.Provider,
+			Service:   res.Service,
+			StartLine: res.StartLine,
+			EndLine:   res.EndLine,
+			Code:      res.Code,
+		},
+	}
+}
+
